fba33cf7d5aa6f6e30439a335b75d54367b0de68
Julius Parishy <hello@juliusparishy.com>
2015-12-23 22:54:38 -0500

    use the remove swerver package dependency

--- Package.swift
+++ Package.swift
@@ -2,6 +2,6 @@ import PackageDescription
 
 let package = Package(
 	dependencies: [
-		.Package(url: "../Swerver", majorVersion: 1)
+		.Package(url: "https://github.com/jparishy/Swerver", majorVersion: 1)
 	]
 )
